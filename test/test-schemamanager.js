4321380384ee851de88599ea9c8df9822c5118f9
Sean Mountcastle <smountcastle@verisign.com>
2013-10-04 13:09:52 -0400

    Added test-server.js to test out the server API.

--- test/test-schemamanager.js
+++ test/test-schemamanager.js
@@ -48,33 +48,35 @@ describe('SchemaManager', function() {
     });
 
 
-  it("should error adding a schema with no name ", function(done) {
-    var name = "";
-    var schema = { "field1" : "String", "field2" : "String" };
-    schemaManager.addSchema({"name" : name, "definition" : schema}, function(err, entity) {
-      should.exist(err);
-      done();
+    it("should error adding a schema with no name ", function(done) {
+      var name = "";
+      var schema = { "field1" : "String", "field2" : "String" };
+      schemaManager.addSchema({"name" : name, "definition" : schema}, function(err, entity) {
+        should.exist(err);
+        done();
+      });
     });
   });
 
-});
+  describe('add-valid-schema', function() {
+    it("should add a valid json schema object", function(done) {
+      var name = "network_element";
+      var schema = {
+        ne_type: "String",
+        cid: "String",
+        ip: "String",
+        ip6: "String",
+        bgpip: "String",
+        bgpip6: "String",
+      }
+      schemaManager.addSchema({"name" : name, "definition" : schema}, function(err, entity) {
+        should.not.exist(err);
 
-describe('add-valid-schema', function() {
-  it("should add a valid json schema object", function(done) {
-    var name = "network_element";
-    var schema = {
-      ne_type: "String",
-      cid: "String",
-      ip: "String",
-      ip6: "String",
-      bgpip: "String",
-      bgpip6: "String",
-    }
-    schemaManager.addSchema({"name" : name, "definition" : schema}, function(err, entity) {
-      should.not.exist(err);
-      done();
-      // figure out how to check the entity
+        entity.should.have.property('name', 'network_element');
+        entity.should.have.property('definition');
+        entity['definition'].should.eql(schema);
+        done();
+      });
     });
   });
-});
 });
