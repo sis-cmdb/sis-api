d55542c0ef783a72942d027ec45c9c8a26557085
Neel Goyal <ngoyal@verisign.com>
2013-12-16 11:24:31 -0500

    Add test initialization script

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 test:
+	mocha test/init-tests.js
 	for test in `ls test/test-*`; do mocha --timeout 6000 $$test; done
 
 .PHONY: test
---
+++ test/init-tests.js
@@ -0,0 +1,46 @@
+/***********************************************************
+
+ The information in this document is proprietary
+ to VeriSign and the VeriSign Product Development.
+ It may not be used, reproduced or disclosed without
+ the written approval of the General Manager of
+ VeriSign Product Development.
+
+ PRIVILEGED AND CONFIDENTIAL
+ VERISIGN PROPRIETARY INFORMATION
+ REGISTRY SENSITIVE INFORMATION
+
+ Copyright (c) 2013 VeriSign, Inc.  All rights reserved.
+
+ ***********************************************************/
+
+var config = require('./test-config');
+var server = require("../server")
+var should = require('should');
+var request = require('supertest');
+var async = require('async');
+
+var mongoose = null;
+var app = null;
+var httpServer = null;
+
+var SIS = require("../util/constants")
+
+describe('Initialize Tests', function() {
+    before(function(done) {
+        server.startServer(config, function(expressApp, httpSrv) {
+            mongoose = server.mongoose;
+            app = expressApp;
+            httpServer = httpSrv;
+            done();
+        });
+    });
+
+    after(function(done) {
+        server.stopServer(httpServer, function() {
+            mongoose.connection.db.dropDatabase();
+            mongoose.connection.close();
+            done();
+        });
+    });
+});
