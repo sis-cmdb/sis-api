3d15c5a6e006fce48653d16164402ede52e3671c
Neel Goyal <ngoyal@verisign.com>
2014-04-13 21:58:36 -0400

    Add version and build id to build info file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -75,13 +75,22 @@ module.exports = function(grunt) {
 
   grunt.registerTask('buildjson', function(target) {
     var outfile = 'build.json';
-    var buildNum = process.env['BUILD_NUMBER'] || 'local-build';
-    var githash = process.env['GIT_COMMIT_HASH'] || 'dev-hash';
-    grunt.file.write(outfile, JSON.stringify({ build_num : buildNum, git_hash : githash }));
+    var buildNum = process.env.BUILD_NUMBER || 'local-build';
+    var githash = process.env.GIT_COMMIT_HASH || 'dev-hash';
+    var buildId = process.env.BUILD_ID || grunt.template.date(Date.now(), 'yyyy-mm-dd_HH-MM-ss');
+
+    var output = {
+        build_num : buildNum,
+        git_hash : githash,
+        build_id : buildId,
+        version : grunt.config.get('pkg.version')
+    };
+    output = JSON.stringify(output);
+    grunt.file.write(outfile, output);
     if (target == 'dist') {
         // write to dist as well
         outfile = 'dist/' + outfile;
-        grunt.file.write(outfile, JSON.stringify({ build_num : buildNum, git_hash : githash }));
+        grunt.file.write(outfile, output);
     }
   });
 
